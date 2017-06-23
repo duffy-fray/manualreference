85d7b962ce51d168b59601d26334fa358d8579af
duffy-fray <duffy.fray@snapav.com>
2017-06-23 11:16:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Duffy Fray
+// fray.duffy@gmail.com
+
 var express = require('express');
 var app = express();
 
@@ -21,10 +24,10 @@ app.use(express.static(__dirname+"/public/views"));
 var globalBodyText = "instantiated text";
 var globalAddress = "fray.duffy@gmail.com";
 var globalRosterName = "";
-//var globalPlayersNames = [];
 var globalRosterObject;
 var globalRosterListForEmail=[];
 
+// Email certificates expire periodically - update here.
 var emailCerts = {
 	clientID: '408979123361-4laud587e6ljcdgah44u8hgjg0dbm7kl.apps.googleusercontent.com',
 	clientSecret: 'Q75MC841i7G4j8QQbnV6A5wV',
@@ -34,6 +37,7 @@ var emailCerts = {
 
 var EmailTemplate = require('email-templates').EmailTemplate;
 
+// Transporter used for nodemailer - enables SMTP functions
 var transporter = nodemailer.createTransport({
 	host: 'smtp.gmail.com',
 	port: 465,
@@ -53,14 +57,14 @@ app.listen(3000, function(){
 	console.log('Server listening on port 3000...\n');
 });
 
+// Retrieves list of teams, presents to view. Used with team view
 app.get('/teamlist', function (req, res){
-	//console.log("Server rec'd a GET request...");
 	dbteamlist.teamlist.find(function (err, docs){
-		//console.log(docs);
 		res.json(docs);
 	})
 });
 
+// Retrieves specific team, presents to view.  Used with team view's "edit" button
 app.get('/teamlist/:id', function (req, res){
 	var id = req.params.id;
 	dbteamlist.teamlist.findOne({_id: mongojs.ObjectID(id)}, function (err, doc){
@@ -68,6 +72,7 @@ app.get('/teamlist/:id', function (req, res){
 	});
 });
 
+// Adds new team
 app.post('/teamlist', function (req, res){
 	console.log("New team added\n\t"+req.body.name);
 	dbteamlist.teamlist.insert(req.body, function(err, doc){
@@ -75,6 +80,7 @@ app.post('/teamlist', function (req, res){
 	});
 });
 
+// Delete specific team upon request.  Used for "delete" button.
 app.delete('/teamlist/:id', function (req, res){
 	var id = req.params.id;
 	console.log("\nServer rec'd DELETE req for TEAM \t"+id+"\n");
@@ -83,6 +89,7 @@ app.delete('/teamlist/:id', function (req, res){
 	});
 });
 
+// Updates team name.  Used for team view's "update" button
 app.put('/teamlist/:id', function (req, res){
     var id = req.params.id;
     console.log(req.body.name);
@@ -94,28 +101,27 @@ app.put('/teamlist/:id', function (req, res){
     })
 });
 
+// Retrieves list of players in database.  Presents to player view
 app.get('/playerlist', function (req, res){
-	//console.log("Server rec'd app.get for playerlist");
-
 	dbplayerlist.playerlist.find(function (err, docs){
-		//console.log(docs);
 		res.json(docs);
-	})
+	});
 });
 
 app.get('/playerManagement.html', function (req, res){
-	console.log("Server trying to serve playerManagement.html");
+	console.log("Serving playerManagement.html");
 	res.send('/playerManagement.html');
-})
+});
 
+// Adds new player to player database.  Used in player view's "add" button
 app.post('/playerlist', function (req, res){
 	dbplayerlist.playerlist.insert(req.body, function(err, doc){
 		res.json(doc);
 		console.log(req.body.name+" added\n");
 	});
-
 });
 
+// Delete player from player database.  Used in player view's "delete" button
 app.delete('/playerlist/:id', function (req, res){
 	var id = req.params.id;
 	dbplayerlist.playerlist.remove({_id: mongojs.ObjectID(id)}, function (err, doc){
@@ -124,14 +130,16 @@ app.delete('/playerlist/:id', function (req, res){
 	});
 });
 
+// Adds new player to database.  Used in player view's "add" button
 app.post('/playerlist', function (req, res){
 	console.log("\t"+req.body.name+"\n");
 	dbplayerlist.playerlist.insert(req.body, function(err, doc){
 		res.json(doc);
 		console.log("Added player\t"+req.body.name+"\n");
-	})
+	});
 });
 
+// Finds specific player in player database.  Used in player view's "edit" button
 app.get('/playerlist/:id', function (req, res){
 	var id = req.params.id;
 	dbplayerlist.playerlist.findOne({_id: mongojs.ObjectID(id)}, function (err, doc){
@@ -140,6 +148,8 @@ app.get('/playerlist/:id', function (req, res){
 	});
 });
 
+//  Updates specific player in player database.  Used in player view's "update" button, used in trading modal,
+//  and used when updating player stats in modal
 app.put('/playerlist/:id', function (req, res){
     var id = req.params.id;
     var teamIDUpdate;
@@ -169,6 +179,7 @@ app.put('/playerlist/:id', function (req, res){
     });
 });
 
+// Retrieves all players from the database for a specific team
 app.get('/rosterFinder/:id', function (req, res){
 	var id = req.params.id;
 	//find just players who's team_id matches team's _id.
@@ -179,13 +190,6 @@ app.get('/rosterFinder/:id', function (req, res){
 		globalRosterName = docs[0].team_name;
 
 		console.log("Sent roster for " + globalRosterName+" from server to view.\n");
-
-/*
-		globalBodyText="";
-		for (var i = docs.length - 1; i >= 0; i--) {
-			globalPlayersNames[i] = docs[i].name
-		}	
-*/
 		
 		for (var i = docs.length - 1; i >= 0; i--) {
 			globalRosterListForEmail[i] = String.fromCharCode(13)+docs[i].name;
@@ -198,7 +202,7 @@ app.get('/contactUs.html', function (req, res){
 	res.send('/contactUs.html')
 });
 
-//nodemailer section
+//  nodemailer test section
 app.get('/nodemailerTestPage', function(req, res){
 	console.log("app.get('/nodemailerTestPage'");
 
@@ -214,7 +218,6 @@ app.get('/nodemailerTestPage', function(req, res){
 		if(err){
 			console.log('\n\tError sending test Email');
 			console.log("\n\t");
-			//console.log(err);	
 			res.json(err);
 
 		} else{
@@ -224,7 +227,7 @@ app.get('/nodemailerTestPage', function(req, res){
 	});
 });
 
-
+// Email a given team's roster to a specified recipient
 app.get('/emailRecipientPage/:recipient', function(req, res){
 	globalAddress = req.params.recipient;
 
@@ -277,85 +280,7 @@ app.get('/emailRecipientPage/:recipient', function(req, res){
 			}
 		});
 	});
-
-	/*
-	var mailOptions = {
-		from: 'Mean App',
-		to: recipient,
-		subject: 'MEAN App',
-		html: { path: 'public/emailFile.html' }
-	};
-
-	console.log("\n\nEmail will go to\t");
-	console.log(mailOptions.to);
-
-	
-	transporter.sendMail(mailOptions, function (err, sendMailResponse){
-		console.log("\n\nSending email..");
-		console.log("\t..");
-		console.log("\t\t..");
-		if(err){
-			console.log('\n\tError sending Email\n\t Check refresh token and access token');
-			console.log("\n\t");
-			console.log("https://developers.google.com/oauthplayground/?code=4/xe7uvCDkbqAGP2_0Ow13A5z4bQOwbg4nlMJ4sByHq6E#");
-			console.log(err);	
-			res.json(err);
-
-		} else{
-			res.json(sendMailResponse);
-			console.log("\t\t\tsuccess");
-		}
-	});
-	*/
 });
 
-/*  DYNAMIC HTML EMAIL WORKIGN EXAMPLE
-var readHTMLFile = function(path, callback){
-	fs.readFile(path, {encoding: 'utf-8'}, function (err, html){
-		if(err){
-			throw err;
-			callback(err);
-		}
-		else {
-			callback(null, html);
-		}
-	});
-};
-
-readHTMLFile(__dirname+'/public/emailFile.html',function(err, html){
-	var template = handlebars.compile(html);
-	var replacements={
-		username: "John Doe"
-	};
-	var htmlToSend = template(replacements);
-	var mailOptions = {
-		from: 'duff.fray@gmail.com',
-		to: 'duffy.fray@snapav.com',
-		subject: 'Dynamic HTML test',
-		html: htmlToSend
-	};
-
-	transporter.sendMail(mailOptions, function(error, response){
-		if(error){
-			console.log(error);
-			callback(error);
-		}
-	});
-});
-
-*/
-/*  DON"T MESS WITH THE TRANSPORTER!!!!!
-const transporter = nodemailer.createTransport({
-	host: 'smtp.gmail.com',
-	port: 465,
-	secure: true,
-	auth: {
-		type: 'OAuth2',
-		user: 'duff.fray@gmail.com',
-		clientID: emailCerts.clientID,
-		clientSecret: emailCerts.clientSecret,
-		refreshToken: emailCerts.refreshToken,
-		accessToken: emailCerts.accessToken
-	}
-});
-*/
+//  Thank you for looking.
+//      -Duffy
