7de465eb2ef4f0f0299ed98015f252948f1f5dfd
duffy-fray <duffy.fray@snapav.com>
2017-06-26 11:55:27 -0400

    Update controller.js
    Added context to comments

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,26 +1,17 @@
-
 var myApp = angular.module('myApp',[]);
 
 myApp.controller('AppCtrl',['$scope', '$rootScope','$http', '$location',function($scope, $rootScope, $http, $location) {
 
+    // Exposes endpoint to access list of player objects
     $http.get('/playerlist').then(function(response){
         $scope.playerlist=response.data;
         $scope.player ={};
     });
-
+    
+    // Exposes endpoint to access list of team objects
     $http.get('/teamlist').then(function(response){
         $scope.teamlist=response.data;
         $scope.team ={};
     });
 }]);
 
-
-/*
-1)  Change player stats from modal - done
-2)  Launch modal to add player with details at same time 
-3)  Business logic
-    a.  Take collection – email out a team and their roster.
-    b.  SMTP – (will need library)
-    c.  Build dynamic HTML to populate team roster
-    d.  Populate from, to, subject, etc.
-*/
